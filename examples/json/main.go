6cbe85022eeb3a5eeee6a6d2ab536c342b4a2327
Dzyanis Kuzmenka <dzyanis@gmail.com>
2021-05-31 22:26:27 +0300

    fixed example

--- examples/json/main.go
+++ examples/json/main.go
@@ -16,12 +16,14 @@ func (u User) Valid() (bool, validator.Errors) {
 	return validator.ByJSON(u, validator.Validators{
 		"name":    validator.StringRequire(u.Name, validator.Shorter(2)),
 		"email":   validator.StringRequire(u.Email, validator.Email()),
-		"message": validator.StringOption(u.Name, validator.Shorter(10)),
+		"message": validator.StringOption(u.Message, validator.Longer(10)),
 	})
 }
 
 func main() {
-	user := User{}
+	user := User{
+		Message: "Oops!",
+	}
 
 	ok, errs := user.Valid()
 	fmt.Printf("validation: %v %v", ok, errs)
